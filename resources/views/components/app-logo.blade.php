d35bc51f9b361c5f58d5ba3d4a6c3531e7e74d88
Norbert Fröhler <norbert@nb086>
2026-02-25 00:36:02 +0100

    .

--- resources/views/components/app-logo.blade.php
+++ resources/views/components/app-logo.blade.php
@@ -7,7 +7,7 @@
         <x-slot name="logo">
             <img src="https://ml6ymizwmc75.i.optimole.com/w:150/h:89/q:mauto/f:best/https://www.buerger-fuer-garching.de/wp-content/uploads/2025/08/Logo-small.png"
                  alt="Bürger für Garching"
-                 class="h-8 w-auto">
+                 class="h-6 w-auto">
         </x-slot>
     </flux:sidebar.brand>
 @else
@@ -15,7 +15,7 @@
         <x-slot name="logo">
             <img src="https://ml6ymizwmc75.i.optimole.com/w:150/h:89/q:mauto/f:best/https://www.buerger-fuer-garching.de/wp-content/uploads/2025/08/Logo-small.png"
                  alt="Bürger für Garching"
-                 class="h-8 w-auto">
+                 class="h-6 w-auto">
         </x-slot>
     </flux:brand>
 @endif
