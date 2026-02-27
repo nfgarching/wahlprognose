bb3e722f26a1e5c0e991ecd69a61ba8562efcc58
Norbert (ws088) <norbert.froehler@gmail.com>
2026-02-27 20:40:38 +0100

    .

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,15 +15,24 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+
+        if (! User::where('email', 'norbert.froehler@gmail.com')->first()) {
+            User::factory()->create([
+                'name' => 'Test User',
+                'email' => 'norbert.froehler@gmail.com',
+                'password' => \Illuminate\Support\Facades\Hash::make('v1adimiR'),
+                'is_admin' => True,
+            ]);
+        }
 
         $this->call([
             PartySeeder::class,
             CandidateSeeder::class,
-            ForecastSeeder::class,
+            // ForecastSeeder::class,
         ]);
     }
 }
